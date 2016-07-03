b161970bc74d5c4074514c484b0a6148c9cfb050
Thorbjørn Nielsen <torybash@gmail.com>
2016-07-03 19:57:52 +0200

    First commit!

---
+++ Assets/DatMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class DatMovement : MonoBehaviour {
+
+	private Rigidbody2D rigiB;
+	private Rigidbody2D RigiB {
+		get {
+			if (rigiB == null) rigiB = GetComponent<Rigidbody2D>();
+			return rigiB;
+		}
+	}
+
+	[SerializeField] private float wheelSpeed = 10;
+
+	private float xIn;
+
+	
+	void Update () {
+		InputMovement();
+	}
+
+	void FixedUpdate() {
+		RigiB.AddTorque(-xIn * wheelSpeed);
+	}
+
+	private void InputMovement() {
+
+		xIn = Input.GetAxis("Horizontal");
+
+		
+	}
+}
